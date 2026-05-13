ab5097dbf10f29e3b8286f883aef01c382ebf12a
joelsanchhhez <joel@siliuto.com>
2026-05-13 20:55:22 +0200

    Desarrollar clase App

---
+++ src/main/java/com/proyectodnd/App.java
@@ -0,0 +1,28 @@
+package java.com.proyectodnd;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Guerrero guerrero = new Guerrero("Thorin");
+        Mago mago = new Mago("Merlín");
+
+        System.out.println("=== Información de los personajes ===");
+        guerrero.mostrarInfo();
+        System.out.println();
+        mago.mostrarInfo();
+
+        System.out.println();
+        System.out.println("=== Combate ===");
+
+        guerrero.atacar(mago);
+        mago.lanzarHechizo(guerrero);
+        guerrero.ataqueEspecial(mago);
+
+        System.out.println();
+        System.out.println("=== Estado final ===");
+        guerrero.mostrarInfo();
+        System.out.println();
+        mago.mostrarInfo();
+    }
+}
